a1f80901ed13eaddf94efc395fa7d0c023a45e3c
Boris <fedotom2@gmail.com>
2019-11-16 11:29:54 +0200

    Lesson 8. Update

--- lesson8-js/js/script.js
+++ lesson8-js/js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-alert('Меня загрузили!');
+// alert('Меня загрузили!');
 
 const btnHandler = () => {
   alert('Привет, друг!');
@@ -31,35 +31,59 @@ const btn1Handler = () => {
 };
 
 const btn2Handler = () => {
-
+  if (block.style.backgroundColor === 'rgb(0, 255, 0)' || block.style.backgroundColor === '')
+    block.style.backgroundColor = 'rgb(255, 0, 0)';
+  else 
+    block.style.backgroundColor = 'rgb(0, 255, 0)';
 };
 
 const btn3Handler = () => {
-
+  if (block.style.borderColor === 'rgb(0, 0, 0)' || block.style.borderColor === '')
+    block.style.borderColor = 'rgb(0, 0, 255)';
+  else 
+    block.style.borderColor = 'rgb(0, 0, 0)';
 };
 
 const btn4Handler = () => {
-
+  if (block.style.borderWidth === '1px' || block.style.borderWidth === '')
+    block.style.borderWidth = '10px';
+  else 
+    block.style.borderWidth = '1px';
 };
 
 const btn5Handler = () => {
-
+  if (block.style.width === '200px' || block.style.width === '')
+    block.style.width = '400px';
+  else 
+    block.style.width = '200px';
 };
 
 const btn6Handler = () => {
-
+  if (block.style.height === '100px' || block.style.height === '')
+    block.style.height = '200px';
+  else 
+    block.style.height = '100px';
 };
 
 const btn7Handler = () => {
-
+  if (block.style.marginLeft === '0px' || block.style.marginLeft === '')
+    block.style.marginLeft = '100px';
+  else 
+    block.style.marginLeft = '0px';
 };
 
 const btn8Handler = () => {
-
+  if (block.style.marginTop === '0px' || block.style.marginTop === '')
+    block.style.marginTop = '100px';
+  else 
+    block.style.marginTop = '0px';
 };
 
 const btn9Handler = () => {
-
+  if (block.style.fontSize === '14px' || block.style.fontSize === '')
+    block.style.fontSize = '30px';
+  else 
+    block.style.fontSize = '14px';
 };
 
 
